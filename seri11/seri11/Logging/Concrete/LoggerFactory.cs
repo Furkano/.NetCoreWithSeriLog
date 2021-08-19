19a8dae60dc08e37f4ffe8bbd743ba2eef8ce79b
Furkan Soysal <f.soysal.93@gmail.com>
2021-08-19 09:35:04 +0300

    Add files via upload

---
+++ seri11/seri11/Logging/Concrete/LoggerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace seri11.Logging.Concrete
+{
+    public class LoggerFactory
+    {
+        private LoggerFactory()
+        {
+
+        }
+        static readonly object _lock = new object();
+        private static MongoSeriLogger _mongoSeriLogger;
+
+        public static MongoSeriLogger MongoLogManager()
+        {
+            if (_mongoSeriLogger == null)
+            {
+                lock (_lock)
+                {
+                    if (_mongoSeriLogger == null)
+                    {
+                        _mongoSeriLogger = new MongoSeriLogger();
+                    }
+                }
+            }
+            return _mongoSeriLogger;
+        }
+    }
+}
